f366dc617bc4a73839c60f4c33e04207f6b6cd78
frisch <frisch@cloudera.com>
2023-03-08 17:37:39 +0100

    feat(model): deployment of model made

--- predict_model.py
+++ predict_model.py
@@ -5,14 +5,19 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.feature import StringIndexer
 from pyspark.ml.classification import DecisionTreeClassificationModel
 import json
+import mlflow.spark
 
-# Create a Spark Session
+
+# Create a Spark Session locally (to avoid spinning up executors)
 spark = SparkSession\
     .builder\
     .appName("WeatherModel")\
+    .config("spark.jars.packages", "org.mlflow:mlflow-spark:2.2.1")\
+    .master("local[*]")\
     .getOrCreate()
+
 # Load Spark model from HDFS
-model_loaded = DecisionTreeClassificationModel.load("model_spark")
+model_loaded = mlflow.spark.load_model("model-spark-mlflow")
 
 columns = ['wind_provenance_9_am' ,  'wind_force_9_am', "wind_provenance_9_pm", "wind_force_9_pm", "pressure_9_am", "pressure_9_pm","humidity_9_am", "humidity_9_pm", "temperature_9_am", "temperature_9_pm"]
 
@@ -20,8 +25,8 @@ columns = ['wind_provenance_9_am' ,  'wind_force_9_am', "wind_provenance_9_pm", 
 def predict(input_dict):
     
     data = [(input_dict["wind_p_9_am"], input_dict["wind_f_9am"], input_dict["wind_p_9_pm"], input_dict["wind_f_9_pm"], input_dict["p_9am"], input_dict["p_9pm"], input_dict["h_9am"], input_dict["h_9pm"], input_dict["t_9am"], input_dict["t_9pm"])]
-    rdd = spark.sparkContext.parallelize(data)
-    df = spark.createDataFrame(rdd).toDF(*columns)
+
+    df = spark.createDataFrame(data,columns)
 
     # Index wind_provenance columns (to transform string to int)
     df_index_wind_9am = StringIndexer(inputCol="wind_provenance_9_am", outputCol="wind_provenance_9_am_index").fit(df).transform(df)
@@ -41,7 +46,7 @@ def predict(input_dict):
 
     # Return result
     raw_result = predictions.select("prediction").take(1)[0]['prediction']
-    if raw_result == 0:
+    if raw_result <= 0.5:
       result = {"rain" : "no" }
     else:
       result = {"rain" : "yes" }
