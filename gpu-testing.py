ccfb692d1b365d188162aeacfc266ee97eb6ca78
RISCH Francois <francois2.risch-prestataire@laposte.fr>
2024-07-22 12:13:44 +0200

    feat(gpu): add a test for gpu on keras with jax

--- gpu-testing.py
+++ gpu-testing.py
@@ -3,13 +3,69 @@
 !pip install -U "jax[cuda12]"
 
 import os
-os.environ["KERAS_BACKEND"] = "tensorflow"
+# One could set backend to "tensorflow", but that would require compute capability of GPU >= 6.0
+os.environ["KERAS_BACKEND"] = "jax"
+import numpy as np
 import keras
+from keras import layers
+
 
 print(keras.__version__)
 
 from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
+print("Available Device for Tensorflow backend:", device_lib.list_local_devices())
 
 import tensorflow as tf
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+from jax.lib import xla_bridge
+print("Available Device for JAX backend:", xla_bridge.get_backend().platform)
+
+num_classes = 10
+input_shape = (28, 28, 1)
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
+
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = keras.Sequential(
+    [
+        keras.Input(shape=input_shape),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="softmax"),
+    ]
+)
+
+model.summary()
+
+batch_size = 128
+epochs = 15
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
+
